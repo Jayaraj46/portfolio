6ef618b9da4f148f42e5779a0c5a0c2cd322897d
Jayaraj V <22ad021@ritrjpm.ac.in>
2024-05-21 15:15:07 +0530

    remove file runner in app.py

--- app.py
+++ app.py
@@ -228,5 +228,5 @@ def submit():
     conn.close()
     return redirect(url_for('dashboard'))   
 
-if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=False,host='0.0.0.0')
