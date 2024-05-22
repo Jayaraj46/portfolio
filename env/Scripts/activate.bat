a13e8d1f224f202ea6540062c0cf7e1706125d3b
Jayaraj V <22ad021@ritrjpm.ac.in>
2024-05-22 16:33:47 +0530

    env changed

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Profession\Portfolio\flask_portfolio\env
+set VIRTUAL_ENV=D:\Profession\Portfolio\portfolio\env
 
 if not defined PROMPT set PROMPT=$P$G
 
